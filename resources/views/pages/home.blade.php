42686f87931fb3c676fac10cf3da4397a07750aa
surajlifeean <surajjeswara@yahoo.com>
2017-07-21 20:15:34 -0700

    home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/demo.css">
+  <link rel="stylesheet" href="css/footer-distributed-with-contact-form.css">
+
+
+  <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+ 
+
+ /* nav bar*/
+.example3 .navbar-brand {
+  height: 80px;
+}
+
+.example3 .nav >li >a {
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+.example3 .navbar-toggle {
+  padding: 10px;
+  margin: 25px 15px 25px 0;
+}
+
+/* image thubnails */
+
+.row {
+    margin: 80px 0px 80px 0px;
+    }
+
+
+.thumbnail {
+    margin: 10px 10px 10px 10px;
+    -webkit-transform: scale(1, 1);
+    -ms-transform: scale(1, 1);
+    transform: scale(1, 1);
+    transition-duration: 0.3s;
+    -webkit-transition-duration: 0.3s; /* Safari */
+    }
+
+.thumbnail:hover {
+  cursor: pointer;
+  -webkit-transform: scale(1.1, 1.1);
+    -ms-transform: scale(1.1, 1.1);
+    transform: scale(1.1, 1.1);
+    transition-duration: 0.5s;
+    -webkit-transition-duration: 0.3s; /* Safari */
+    box-shadow: 10px 10px 5px #888888;
+    z-index: 1;
+    }
+
+
+  </style>
+
+</head>
+<body>
+
+<div class="example3">
+  <nav class="navbar navbar-light navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+          <span class="glyphicon glyphicon-menu-hamburger"></span>
+          
+        </button>
+        <a href="http://disputebills.com"><img src="images/img3.png" height="90px" alt="Dispute Bills">
+        </a>
+      </div>
+      <div id="navbar3" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="#"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li class="dropdown-header">Nav header</li>
+              <li><a href="#">Separated link</a></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+    <!--/.container-fluid -->
+  </nav>
+</div>
+  </div>
+
+<div class="container">
+
+<!--image slider -->
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="images/banner1.jpg" alt="Los Angeles" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Banarasi  Saree</h3>
+          <p>It is always so much fun!</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="images/banner2.jpg" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Fantastic Kurties</h3>
+          <p>Catchy and glazzy!</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="images/banner3.jpg" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Best Lehngas </h3>
+          <p>Special Collection Available!</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+  <!--image slider ends-->
+<div class="container">
+
+<div class="row">
+  <div class="col-md-3 col-sm-4 col-xs-6">
+    <a href="#" class="thumbnail">
+      <img src="images/kurti.jpg" alt="...">
+    </a>
+    </div>
+  <div class="col-md-3 col-xs-6 col-sm-4 ">
+    <a href="#" class="thumbnail">
+      <img src="images/kurti.jpg" alt="...">
+    </a>
+  </div>
+
+  <div class="col-md-3 col-xs-6 col-sm-4">
+    <a href="#" class="thumbnail">
+      <img src="images/kurti.jpg" alt="...">
+    </a>
+    </div>
+  <div class="col-md-3 col-xs-6 col-sm-4">
+    <a href="#" class="thumbnail">
+      <img src="images/kurti.jpg" alt="...">
+    </a>
+  </div>
+</div>
+</div>
+
+
+    <footer class="footer-distributed">
+
+      <div class="footer-left">
+
+        <h3>Ban<span>surya</span></h3>
+
+        <p class="footer-links">
+          <a href="#">Home</a>
+          ·
+          <a href="#">Blog</a>
+          ·
+          <a href="#">Pricing</a>
+          ·
+          <a href="#">About</a>
+          ·
+          <a href="#">Faq</a>
+          ·
+          <a href="#">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Bansuriya &copy; 2015</p>
+
+        <div class="footer-icons">
+
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+
+        </div>
+
+      </div>
+
+      <div class="footer-right">
+
+        <p>Contact Us</p>
+
+        <form action="#" method="post">
+
+          <input type="text" name="email" placeholder="Email" />
+          <textarea name="message" placeholder="Message"></textarea>
+          <button>Send</button>
+
+        </form>
+
+      </div>
+
+    </footer>
+
+
+</body>
+</html>
